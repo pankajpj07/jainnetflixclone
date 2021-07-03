75a22449a1292b08331fb6e5b7e009987dabf93f
pankajpj07 <pankaj12.pj88@gmail.com>
2021-07-03 11:28:29 +0530

    added jainflix logo and favicon

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./css/Nav.css";
+import logo from "./assets/jainflix_logo.png";
 
 function Nav() {
   const [show, handleshow] = useState(false);
@@ -8,17 +9,13 @@ function Nav() {
       window.scrollY > 100 ? handleshow(true) : handleshow(false);
     });
     return () => {
-      window.removeEventListener("scroll");
+      window.removeEventListener("scroll", null);
     };
   }, []);
 
   return (
     <div className={`nav ${show && "nav_black"}`}>
-      <img
-        className="nav_logo"
-        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-        alt="Jainflix Logo"
-      />
+      <img className="nav_logo" src={logo} alt="Jainflix Logo" />
       <img
         className="nav_avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
